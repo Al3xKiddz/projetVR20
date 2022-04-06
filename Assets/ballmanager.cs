5f01b88e2b47a09c6c7373dac44edeace718de69
Al3xKiddz <70660696+Al3xKiddz@users.noreply.github.com>
2022-04-06 12:16:48 -0400

    fini peut etre

---
+++ Assets/ballmanager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballmanager : MonoBehaviour
+{
+
+    [SerializeField]
+    private AudioSource playerAudio;
+    [SerializeField]
+    private Rigidbody rbBalle;
+
+    private float maxvel = 200f;
+    // Start is called before the first frame update
+    void Awake()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+        if (collision.gameObject.name == "grosbat")
+        {
+            playerAudio.Play();
+            rbBalle.velocity = Vector3.zero;
+            Vector3 dir = (transform.position - collision.contacts[0].point).normalized;
+            rbBalle.AddForce(dir * 12f, ForceMode.Impulse);
+        }
+
+    }
+}
